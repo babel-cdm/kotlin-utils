68931c1e15f47bf1b45b31b0f40dd0e306cf23f8
Jose Manuel Martin Prado <josemanuel.martin@babel.es>
2020-09-24 19:46:26 +0200

    Module structure created
    Why:
        - Cause it's the beginning of the project, and it's needed to be structured.
    How:
        - Creating the needed files and folders.

---
+++ build.gradle.kts
@@ -0,0 +1,76 @@
+import AndroidConfiguration.COMPILE_SDK_VERSION
+import AndroidConfiguration.MIN_SDK_VERSION
+import BuildType.Companion.DEBUG
+import BuildType.Companion.RELEASE
+import dependencies.AndroidTestDependencies.TEST_RUNNER
+import dependencies.Dependencies.KOTLIN
+import dependencies.Dependencies.KTX_CORE
+import dependencies.Dependencies.LOGGING_INTERCEPTOR
+import dependencies.Dependencies.Retrofit
+import dependencies.Dependencies.TIMBER
+import dependencies.TestDependencies.JUNIT
+import dependencies.TestDependencies.MOCKITO
+
+plugins {
+    id(Plugins.ANDROID_LIBRARY)
+    id(Plugins.JACOCO)
+    id(Plugins.JACOCO_REPORT)
+    kotlin(Plugins.KOTLIN_ANDROID)
+    kotlin(Plugins.KOTLIN_ANDROID_EXTENSIONS)
+}
+
+android {
+    compileSdkVersion(COMPILE_SDK_VERSION)
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_1_8.toString()
+    }
+
+    defaultConfig {
+        minSdkVersion(MIN_SDK_VERSION)
+    }
+
+    buildTypes {
+        getByName(DEBUG) {
+            isMinifyEnabled = BuildTypeDebug.isMinifyEnabled
+            isTestCoverageEnabled = BuildTypeDebug.isTestCoverageEnabled
+        }
+        getByName(RELEASE) {
+            isMinifyEnabled = BuildTypeRelease.isMinifyEnabled
+            isTestCoverageEnabled = BuildTypeRelease.isTestCoverageEnabled
+            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
+        }
+    }
+
+    sourceSets {
+        map { sourceSet -> sourceSet.java.srcDir("src/${sourceSet.name}/kotlin") }
+    }
+
+    lintOptions {
+        disable("ObsoleteLintCustomCheck")
+    }
+
+}
+
+dependencies {
+    implementation(KOTLIN)
+    implementation(KTX_CORE)
+
+    implementation(TIMBER)
+
+    implementation(Retrofit.CORE)
+    implementation(Retrofit.GSON)
+    implementation(Retrofit.SCALARS)
+    implementation(Retrofit.COROUTINES_ADAPTER)
+
+    implementation(LOGGING_INTERCEPTOR)
+
+    testImplementation(JUNIT)
+    testImplementation(MOCKITO)
+    androidTestImplementation(TEST_RUNNER)
+}
