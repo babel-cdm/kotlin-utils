a6ca5560beb978ff192ee1261f84a86594fe3fb3
Jose Manuel Martin Prado <josemanuel.martin@babel.es>
2020-09-29 14:25:09 +0200

    Date utils added
    Why:
        - Cause the applications used to need this kind of utils.
    How:
        - Adding Date and String class extensions.
        - Adding Date constants.

---
+++ src/main/kotlin/es/babel/cdm/utils/constants/Date.kt
@@ -0,0 +1,21 @@
+package es.babel.cdm.utils.constants
+
+object Pattern {
+    const val YEAR = "YYYY"
+    const val MONTH = "MM"
+    const val MONTH_OF_THE_YEAR = "MMMM"
+    const val DAY = "dd"
+    const val DAY_OF_THE_WEEK = "EEEE"
+    const val HOUR = "HH"
+    const val MINUTE = "mm"
+    const val SECOND = "ss"
+    const val FULL_DATE_AND_HOUR = "dd/MM/yyyy HH:mm:ss"
+    const val FULL_DATE = "dd/MM/yyyy"
+    const val MONTH_AND_DAY = "dd/MM"
+    const val FULL_HOUR = "HH:mm:ss"
+    const val HOUR_AND_MINUTE = "HH:mm"
+}
+
+object TimeZone {
+    const val UTC = "UTC"
+}
