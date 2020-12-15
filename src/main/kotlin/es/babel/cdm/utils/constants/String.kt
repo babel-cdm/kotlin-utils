c2a17e5204ea834a6cbd461eac8e2ad83561c83b
José Manuel Martín Prado <josemanuel.martin@babel.es>
2020-12-15 14:54:52 +0100

    String utils updated
    Why:
        - Cause new constants are needed.
    How:
        - Updating the date constants file.

--- src/main/kotlin/es/babel/cdm/utils/constants/String.kt
+++ src/main/kotlin/es/babel/cdm/utils/constants/String.kt
@@ -3,4 +3,10 @@ package es.babel.cdm.utils.constants
 object String {
     const val BLANK = " "
     const val DASH = "-"
+    const val COMMA = ","
+
+    object Format {
+        const val DECIMAL_NUMBER = "#.00"
+        const val PRICE = "%s €"
+    }
 }
