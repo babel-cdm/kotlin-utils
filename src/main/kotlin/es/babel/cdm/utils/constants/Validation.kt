3b5a517f6a5b57d9d596a39aedffe20cf9658963
Ana Polo Sánchez <>
2020-12-16 13:08:20 +0100

    Deleted CIF validation. Modified tests

--- src/main/kotlin/es/babel/cdm/utils/constants/Validation.kt
+++ src/main/kotlin/es/babel/cdm/utils/constants/Validation.kt
@@ -6,8 +6,7 @@ object Validation {
             "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d\$€\\[\\]<>@#&*¿?¡!|\\/\\\\\\-_]{8,16}\$"
         const val PHONE = "^[0-9]{4,14}\$"
         const val SPANISH_PHONE = "^[0-9]{9}\$"
-        const val NIE = "^[XxTtYyZz]{1}[0-9]{7}[a-zA-Z]{1}\$"
-        const val CIF = "^[a-zA-Z]{1}\\d{7}[a-zA-Z0-9]{1}\$"
+        const val NIE = "^[XxYyZz]{1}[0-9]{7}[a-zA-Z]{1}\$"
         const val DNI = "^\\d{8}[a-zA-Z]{1}\$"
     }
 }
