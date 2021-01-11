150e7a7bddb928fbc62673e029d7928849739ab9
Jorge <jorge.guilabert@babel.es>
2021-01-11 09:41:20 +0100

    Created Bytearrray extensions
    Why:
        - Utility for parse Bytearray object to File object
    How:
        - Created ByteArray extensions file
        - Created ApplicationType constants file to open the application by Intent.

---
+++ src/main/kotlin/es/babel/cdm/utils/constants/ApplicationType.kt
@@ -0,0 +1,8 @@
+package es.babel.cdm.utils.constants
+
+object ApplicationType {
+    const val PDF = "application/pdf"
+    const val WORD = "application/msword"
+    const val EXCEL = "application/vnd.ms-excel"
+    const val PK_PASS = "application/pkpass"
+}
---
+++ src/main/kotlin/es/babel/cdm/utils/extensions/ByteArray.kt
@@ -0,0 +1,12 @@
+package es.babel.cdm.utils.extensions
+
+import es.babel.cdm.utils.constants.String.BAR
+import java.io.File
+import java.util.*
+
+fun ByteArray.toFile(directory: String, fileName: String? = null): File {
+    val file = File(directory + BAR + (fileName ?: UUID.randomUUID()))
+    file.writeBytes(this)
+
+    return file
+}
